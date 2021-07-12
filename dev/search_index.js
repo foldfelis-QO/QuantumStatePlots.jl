9eb977385f0d5d3f39ddcd8de1bc85b8defa5e07
Documenter.jl <documenter@juliadocs.github.io>
2021-07-12 05:40:13 +0000

    build based on 5f94b2e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuantumStatePlots","category":"page"},{"location":"#QuantumStatePlots","page":"Home","title":"QuantumStatePlots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuantumStatePlots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuantumStatePlots]","category":"page"}]
+}
