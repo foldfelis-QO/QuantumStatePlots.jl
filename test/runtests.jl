0af83d3e75263f777498c7748f3db3fea2e828f4
JingYu Ning <foldfelis@gmail.com>
2022-03-09 21:12:19 +0800

    Test all functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,42 @@
 using QuantumStatePlots
+using Plots
 using QuantumStateBase
+using VisualRegressionTests
 using Test
 
 @testset "QuantumStatePlots.jl" begin
 
 ENV["GKSwstype"]="nul"
 
-@testset "plot wigner" begin
+ρ = SqueezedState(0.8, π/8, Matrix, dim=100)
+w = wigner(ρ, LinRange(-3, 3, 101), LinRange(-3, 3, 101))
 
-end
+# ##########
+# # wigner #
+# ##########
 
-@testset "plot ρ" begin
+@plottest begin
+    surface(w)
+end "assets/surface.png"
 
-end
+@plottest begin
+    heatmap(w)
+end "assets/heatmap.png"
+
+@plottest begin
+    contour(w)
+end "assets/contour.png"
+
+# #####
+# # ρ #
+# #####
+
+@plottest begin
+    plot_real(ρ, 35)
+end "assets/real.png"
+
+@plottest begin
+    plot_imag(ρ, 35)
+end "assets/imag.png"
 
 end
